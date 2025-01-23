6b533a7f401f4fc967537fb3d8f94f32c346bb88
Bhawna Mehbubani <bhawnamehbubani@gmail.com>
2025-01-24 02:16:59 +0530

    Update  Sorted Squared Array.py

--- Day 1/ Sorted Squared Array.py
+++ Day 1/ Sorted Squared Array.py
@@ -2,7 +2,8 @@
 Coding Exercise: Sorted Squared Array
 
 Question
-You are given an array of Integers in which each subsequent value is not less than the previous value. Write a function that takes this array as an input and returns a new array with the squares of each number sorted in ascending order.
+You are given an array of Integers in which each subsequent value is not less than the previous value. 
+Write a function that takes this array as an input and returns a new array with the squares of each number sorted in ascending order.
 Remember
 You can solve this question in multiple ways.
 Think about the following -
