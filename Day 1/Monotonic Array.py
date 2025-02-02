5081f20e251e360bc9b06ee2b5f258dbe019955b
Bhawna Mehbubani <bhawnamehbubani@gmail.com>
2025-02-02 23:29:13 +0530

    Update Monotonic Array.py

--- Day 1/Monotonic Array.py
+++ Day 1/Monotonic Array.py
@@ -7,21 +7,29 @@ An array is monotone decreasing if all  its elements from left to right are non-
 Given an integer array return true if the given array is monotonic, or false otherwise.
 '''
 def monotonic_array(array):
-    first = array[0]
-    last = array[len(array)-1]
-    if first > last:
-        for i in range(len(array)-1):
-            if array[i] < array[i+1]:
+    #write code here
+    n=len(array)
+    if n==0: return True
+    if n==1: return True
+    first=array[0]
+    last=array[n-1]
+    if first>last:
+        for k in range(n-1):
+            #MD or not monotonic
+            if array[k]<array[k+1]:
                 return False
     elif first == last:
-        for i in range(len(array)-1):
-            if array[i] != array[i+1]:
+        for k in range(n-1):
+            #MD if all values are same
+            if array[k]!=array[k+1]:
                 return False
     else:
-        for i in range(len(array)-1):
-            if array[i] > array[i+1]:
+        for k in range(n-1):
+            #MI or not monotonic
+            if array[k]>array[k+1]:
                 return False
     return True
+        
 
 
 print(monotonic_array([1]))
